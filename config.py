3115aa90bd0360b49200169c8de536aac6ccdb71
Daniel <danielyum55@gmail.com>
2025-03-25 23:18:48 +0800

    update

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 GOOGLE_CREDENTIALS = os.getenv("GOOGLE_CREDENTIALS_PATH")  # Path to Google Cloud JSON key
-OPENAI_API_KEY = os.getenv("secret")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
